a15f088895bacf2b26d4f3b71537c9c93b52df2c
DevHimanshu63 <hsc95067@gmail.com>
2022-02-12 23:47:57 +0530

    console.table in array

---
+++ array.js
@@ -0,0 +1,39 @@
+car=["himanshu",1,2,'annu']
+console.log(car)
+
+car[1]="pushpa"
+console.log(car)
+
+
+// methods in array
+car.pop()
+console.log(car)
+
+car.push("darling")
+console.log(car)
+
+
+// unshift method--> this adds a element at the starting of tha Array
+
+car.unshift("annujanu")
+console.log(car)
+
+// shift method--> it removes Element from 0th index of aaray
+
+car.shift()
+console.log(car)
+console.log(car.length)
+
+
+// 2d array
+
+
+let aaray2d=[   
+        [1,1,1],
+        [2,3,5],
+        [8,9,50]
+
+]
+console.log(aaray2d)
+console.table(aaray2d)
+console.log(aaray2d[1][1])

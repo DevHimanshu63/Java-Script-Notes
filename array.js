45baefc4b65764089798270b32672e13b748e834
DevHimanshu63 <hsc95067@gmail.com>
2022-02-13 22:38:50 +0530

    length od 2d araya

--- array.js
+++ array.js
@@ -29,19 +29,33 @@ console.log(car.length)
 // 2d is an array which consist of arrays
 
 let aaray2d=[   
-        [1,1,1],
+        [1,1,1,5],
         [2,3,5],
-        [8,9,50]
+        [8,9,50],
+        [5,6,9]
 
 ]
 // ["a","b","c"]array  of string
  
 console.log(aaray2d)
-console.table(aaray2d)
+console.table(aaray2d) //for table
 
 console.log(aaray2d[1][1])
 
 console.log(aaray2d[1])
+console.log([1][3]);
+
 
 let ans=aaray2d[1]
 console.log(ans[2]);
+
+// for value insert in 2d Array
+
+aaray2d[1][3]=50
+console.table(aaray2d);
+
+// for length in 2d array
+
+console.log(aaray2d.length) //for length of rows 
+console.log(aaray2d[0].length) //for length of columns in this case, we select a row and then count the length
+
